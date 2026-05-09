79346648b0006ec1f195b665b38149cd01632a81
blakepermenter <blakepermenter8@yahoo.com>
2026-05-09 09:38:26 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StatsIQ from './StatsIQ';
+import StatsIQ from './StatsIQ.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
